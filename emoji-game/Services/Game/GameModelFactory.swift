ff331f57b2332f04705263a01dc61ea5db6e1f3f
Vlad Shchuka <shchukavlad4@gmail.com>
2022-01-14 13:40:07 +0200

    Upgrade game models factory algorithm

--- emoji-game/Services/Game/GameModelFactory.swift
+++ emoji-game/Services/Game/GameModelFactory.swift
@@ -30,23 +30,56 @@ final class GameModelFactory {
         from resultImage: UIImage,
         and modelImages: [GameModelFactory.Item]
     ) -> GameModel? {
-        var specs: [GameModel.SpecAsset] = modelImages
-            .map {
-                GameModel.SpecAsset(
-                    truly: true,
-                    image: $0.image,
-                    unicode: $0.unicode
-                )
-            }
-            .uniqued()
+        let specs: [GameModel.SpecAsset] = fill(
+            specs: modelImages
+                .map {
+                    GameModel.SpecAsset(
+                        truly: true,
+                        image: $0.image,
+                        unicode: $0.unicode
+                    )
+                }
+                .uniqued()
+        )
         
+        let trullySpecs = specs
+            .filter { $0.truly == true }
+            .shuffled()
+        
+        let wrongSpecs = specs
+            .filter { $0.truly == false }
+            .shuffled()
+        
+        let topSpecs = [
+            trullySpecs[0],
+            wrongSpecs[0],
+            wrongSpecs[1]
+        ]
+            .shuffled()
+        
+        let bottomSpecs = [
+            trullySpecs.count > 1 ? trullySpecs[1] : wrongSpecs[4],
+            wrongSpecs[2],
+            wrongSpecs[3]
+        ]
+            .shuffled()
+        
+        let top = GamePanelItems(topSpecs[0], topSpecs[1], topSpecs[2])
+        let bottom = GamePanelItems(bottomSpecs[0], bottomSpecs[1], bottomSpecs[2])
+        
+        return GameModel(topPanel: top, bottomPanel: bottom, result: resultImage)
+    }
+}
+
+// MARK: Private
+private extension GameModelFactory {
+    func fill(specs list: [GameModel.SpecAsset]) -> [GameModel.SpecAsset] {
+        var specs = list
         repeat {
-            let list = emojisList.fetchEmojis()
-            let item: EmojiModel? = list.count > 0 ? list[Int.random(in: (0..<list.count))] : nil
             guard
-                let item = item,
+                let item = fetchRandomModel(for: specs),
                 let image = UIImage(named: item.imageName)
-            else { return nil }
+            else { return list }
             specs.append(
                 GameModel.SpecAsset(
                     truly: false,
@@ -54,14 +87,24 @@ final class GameModelFactory {
                     unicode: item.unicode
                 )
             )
-            specs = specs.uniqued()
         } while specs.count < 6
         
         specs.shuffle()
         
-        let top = GamePanelItems(specs[0], specs[1], specs[2])
-        let bottom = GamePanelItems(specs[3], specs[4], specs[5])
+        return specs
+    }
+    
+    func fetchRandomModel(for specs: [GameModel.SpecAsset]) -> EmojiModel? {
+        var model: EmojiModel?
+        let list = emojisList.fetchEmojis()
         
-        return GameModel(topPanel: top, bottomPanel: bottom, result: resultImage)
+        repeat {
+            let fetchedItem: EmojiModel? = list.count > 0 ? list[Int.random(in: (0..<list.count))] : nil
+            if specs.filter({ $0.unicode == fetchedItem?.unicode ?? "" }).count == 0 {
+                model = fetchedItem
+            }
+        } while model == nil
+        
+        return model
     }
 }
